5a320f4141b2fcc18c358236880bbcbecc11e169
Denis <1denrocks1@gmail.com>
2020-10-18 19:07:17 +0300

    added categories endpoint

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const coursesController = require('../controllers/coursesController');
+const Course = require('../models/Course');
 
 const router = express.Router();
 
 router.route('/')
     .get(coursesController.getAllCourses)
     .post(coursesController.addCourse);
+
+router.get('/categories', async (req, res, next) => {
+    const categories = await Course.aggregate([{ $group: { _id: '$category' } }]);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            categories
+        }
+    });
+});
 
 router.route('/:courseId')
     .get(coursesController.getSingleCourse)
